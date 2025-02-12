cf6b8f131a49d4bd5835b6738b84033ca958c4d4
Phan Thanh Duc <67327661+Deadworld-bit@users.noreply.github.com>
2025-02-12 10:04:21 +0700

    Update code
    - Add Navbar
    - Add Review
    - Add Feature
    - Add Footer

---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'about_us', label: 'About Us' },
+    { href: '/', key: 'feature', label: 'Feature' },
+    { href: '/', key: 'membership', label: 'Membership ' },
+    { href: '/', key: 'contact_us', label: 'Contact Us' },
+  ];
+  
+  // OVERVIEW SECTION
+  export const PEOPLE_URL = [
+    '/pics/person-1.png',
+    '/pics/person-2.png',
+    '/pics/person-3.png',
+    '/pics/person-4.png',
+  ];
+  
+  // FEATURES SECTION
+  export const FEATURES = [
+    {
+      title: 'Real maps can be offline',
+      icon: '/icons/map.svg',
+      variant: 'green',
+      description:
+        'aasdasdasdasd ajsdbjabsdjk ajksdbkjasbdjkaba asdaljsdhbnajklsbd asjkdbakjsdbjkasbd jakbsdkjabskdjbakjsbd',
+    },
+    {
+      title: 'Set an adventure schedule',
+      icon: '/icons/calendar.svg',
+      variant: 'green',
+      description:
+        "aasdasdasdasd ajsdbjabsdjk ajksdbkjasbdjkaba asdaljsdhbnajklsbd asjkdbakjsdbjkasbd jakbsdkjabskdjbakjsbd",
+    },
+    {
+      title: 'Technology using augment reality',
+      icon: '/icons/tech.svg',
+      variant: 'green',
+      description:
+        'aasdasdasdasd ajsdbjabsdjk ajksdbkjasbdjkaba asdaljsdhbnajklsbd asjkdbakjsdbjkasbd jakbsdkjabskdjbakjsbd',
+    },
+    {
+      title: 'Many new locations every month',
+      icon: '/icons/location.svg',
+      variant: 'orange',
+      description:
+        'aasdasdasdasd ajsdbjabsdjk ajksdbkjasbdjkaba asdaljsdhbnajklsbd asjkdbakjsdbjkasbd jakbsdkjabskdjbakjsbd',
+    },
+  ];
+  
+  // FOOTER SECTION
+  export const FOOTER_LINKS = [
+    {
+      title: 'Learn More',
+      links: [
+        'About App',
+        'Press Releases',
+        'Environment',
+        'Jobs',
+        'Privacy Policy',
+        'Contact Us',
+      ],
+    },
+    {
+      title: 'Our Community',
+      links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
+    },
+  ];
+  
+  export const FOOTER_CONTACT_INFO = {
+    title: 'Contact Us',
+    links: [
+      { label: 'Admin Officer', value: '123-456-7890' },
+      { label: 'Email Officer', value: 'sold@akinthil.com' },
+    ],
+  };
+  
+  export const SOCIALS = {
+    title: 'Social',
+    links: [
+      '/icons/facebook.svg',
+      '/icons/instagram.svg',
+      '/icons/twitter.svg',
+      '/icons/youtube.svg',
+      '/icons/wordpress.svg',
+    ],
+  };
