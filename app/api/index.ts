5dfd6221acc098ed7baa41be054f2789ec87bfc6
Phan Thanh Duc <67327661+Deadworld-bit@users.noreply.github.com>
2025-03-28 21:14:22 +0700

    update timeout for api

--- app/api/index.ts
+++ app/api/index.ts
@@ -5,7 +5,7 @@ const API_BASE_URL = "https://childgrowthtrackingsystembe-production.up.railway.
 
 const api = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 10000,
+  timeout: 60000,
   headers: {
     "Content-Type": "application/json",
   },
